e7938c6c9e39fa171fa154ed65d5018d0e01c667
guoxiujuan <836832555@qq.com>
2019-04-29 14:55:56 +0800

    5811

---
+++ www/js/lib.js
@@ -0,0 +1,29 @@
+﻿
+$(document).ready(function($) {
+	
+	// 手机导航
+	$('.menuBtn').append('<b></b><b></b><b></b>');
+	$('.menuBtn').click(function(event) {
+		$(this).toggleClass('open');
+		var _winw = $(window).width();
+		var _winh = $(window).height();
+		if( $(this).hasClass('open') ){
+			$('body').addClass('open');
+			if( _winw<=992 ){
+				$('.soBox').stop().slideDown();
+				$('.nav').stop().slideDown();
+			}
+		}else{
+			$('body').removeClass('open');
+			if( _winw<=992 ){
+				$('.soBox').stop().slideUp();
+				$('.nav').stop().slideUp();
+			}
+		}
+	});
+
+
+
+	
+	
+});
